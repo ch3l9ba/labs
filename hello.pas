06134ea52d02eb85c54044c29c016337105dd4d6
Никита Пономарев <ponomarevnikita28674@gmail.com>
2024-10-13 13:02:32 +0300

    Изменил принцип программы

--- hello.pas
+++ hello.pas
@@ -1,1 +1,10 @@
-﻿1
+﻿Program hello;
+begin
+  var lang := ReadString('Введите язык без пробелов:');
+  case lang of
+    'Русский' : PrintLn('Привет!');
+    'Английский' : PrintLn('Hello!');
+    'Итальянский' : PrintLn('Ciao!');
+    else PrintLn('Я такого языка не знаю:(');
+  end;
+end.
